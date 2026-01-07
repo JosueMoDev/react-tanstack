67715c9701296881355516a3e7053043eccef5ad
Jonas Morales <jonasjosuemo2021@outlook.com>
2026-01-07 11:14:00 -0600

    in this commit it has been added axios and tanstack dependencies, and also some folders as interfaces and api

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,9 +1,30 @@
+import { useQuery } from "@tanstack/react-query";
+import { sleep } from "../../helpers";
+
+const getLabels = async (): Promise<unknown[]> => {
+  await sleep(1500);
+  const resp = await fetch(
+    "https://api.github.com/repos/facebook/react/labels"
+  ).then((r) => r.json());
+  console.log(resp);
+  return resp;
+};
 export const LabelPicker = () => {
+  const labelsQuery = useQuery({
+    queryKey: ["labels"],
+    queryFn: getLabels,
+  });
+  if (labelsQuery.isLoading)
+    return (
+      <div>
+        <p>Espere por favor!!...</p>
+      </div>
+    );
   return (
     <>
       <span
         className="px-2 py-1 rounded-full text-xs font-semibold hover:bg-slate-800 cursor-pointer"
-        style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
+        style={{ border: `1px solid #ffccd3`, color: "#ffccd3" }}
       >
         Primary
       </span>
